35b19081b20ba5f3bad8c443dc4d70baa1683d7b
Aman Vishwakarma <amanvishwakarma.va96@gmail.com>
2022-03-20 19:50:35 +0530

    Uploading Project

---
+++ Hospital Portal/Controllers/HospitalController.cs
@@ -0,0 +1,34 @@
+﻿using Hospital_Portal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hospital_Portal.Controllers
+{
+    public class HospitalController : Controller
+    {
+        // GET: Hospital
+        public ActionResult Index()
+        {
+            HospitalDBContext dBContext = new HospitalDBContext();
+            List<tbl_MstHospital> hospitalList = dBContext.tbl_MstHospital.ToList();
+            return View(hospitalList);
+        }
+        [HttpPost]
+        public ActionResult Index(string HospitalName)
+        {
+            HospitalDBContext dBContext = new HospitalDBContext();
+            return View(dBContext.SearchHospital(HospitalName));
+        }
+        
+        public async Task<ActionResult> HospitalDetails(int HospitalId)
+        {
+            ViewBag.PageName = "HospitalDetails";
+            HospitalDBContext dBContext = new HospitalDBContext();
+            return View(dBContext.tbl_MstHospital.Find(HospitalId));
+        }
+    }
+}
